fe8da1e22e1de4a1f29477666a3e8caadc20a9e4
andhana <magicwarms@gmail.com>
2020-05-22 16:50:21 +0700

    add pvid in experience

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,27 +1,49 @@
-import {StudentResolvers} from "../app/controllers/StudentController";
-import {HobbiesResolvers} from "../app/controllers/HobbiesController";
+// import {StudentResolvers} from "../app/controllers/StudentController";
+// import {HobbiesResolvers} from "../app/controllers/HobbiesController";
 
 import path from 'path';
 import fs from 'fs';
 
 //joining path of directory 
-const directoryPath = path.join(__dirname, '../app/controllers');
-console.log(directoryPath)
-const files = fs.readdirSync(directoryPath);
-let contents = '';
-files.forEach(function (file) {
-    contents += fs.readFileSync(`${__dirname}/graphql/${file}`, 'utf8') + ',\n';
-});
+// const directoryPath = path.join(__dirname, '../app/controllers');
+// const files = fs.readdirSync(directoryPath);
 
-const resolvers = {
-    Query: {
-        ...StudentResolvers.Query,
-        ...HobbiesResolvers.Query
-    },
-    Mutation: {
-        ...StudentResolvers.Mutation,
-        ...HobbiesResolvers.Mutation
-    }
-};
+// let resolvers = '';
+// files.forEach(function (file) {
+//     resolvers = fs.readFileSync(`${process.cwd()}/app/controllers/HobbiesController.js`, 'utf8');
+// });
 
-export default resolvers
+let module_holder = {};
+function LoadModules(path) {
+    fs.lstat(path, function (err, stat) {
+        if (stat.isDirectory()) {
+            // we have a directory: do a tree walk
+            fs.readdir(path, function (err, files) {
+                var f, l = files.length;
+                for (var i = 0; i < l; i++) {
+                    f = path_module.join(path, files[i]);
+                    LoadModules(f);
+                }
+            });
+        } else {
+            // we have a file: load it
+            require(path)(module_holder);
+        }
+    });
+}
+
+let DIR = path_module.join(__dirname, 'lib', 'api');
+console.log(LoadModules(DIR))
+
+// const resolvers = {
+//     Query: {
+//         ...StudentResolvers.Query,
+//         ...HobbiesResolvers.Query
+//     },
+//     Mutation: {
+//         ...StudentResolvers.Mutation,
+//         ...HobbiesResolvers.Mutation
+//     }
+// };
+
+// export default resolvers
